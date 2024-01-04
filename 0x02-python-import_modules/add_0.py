150add5b8722fa3413d3f7c06142b9a0b035ee65
Ahmd-Sadka <flookaty@gmail.com>
2024-01-04 15:02:43 -0800

    add

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """This function adds two numbers and returns the result.
+
+    Args:
+        a: The first number.
+        b: The second number.
+
+    Returns:
+        The sum of a and b.
+    """
+    return a + b
